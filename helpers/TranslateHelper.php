90e3c157602576269c3cb3437baf858789a6883b
zapalm <zapalm@ya.ru>
2020-08-14 18:42:45 +1000

    Improved translate() method: added the translations directory parameter

--- helpers/TranslateHelper.php
+++ helpers/TranslateHelper.php
@@ -24,21 +24,23 @@ class TranslateHelper
      * Translates a sentence.
      *
      * It aimed to translate attributes of classes or other literals in any classes.
-     * It will create a translation file in /translations/[ISO code]/sentences.php and after this you can translate literals manually.
+     * It will create a translation file, for example, in `/translations/[ISO code]/sentences.php` and after this you can translate literals manually.
      *
-     * @param string      $sentence        The sentence.
-     * @param string      $class           The class.
-     * @param string|null $languageIsoCode The language ISO code or null to get it automatically from the context.
+     * @param string      $sentence              The sentence.
+     * @param string      $class                 The class.
+     * @param string|null $languageIsoCode       The language ISO code or null to get it automatically from the context.
+     * @param string      $translationsDirectory The directory for translation files. Defaults: `/translations`.
      *
      * @return string The translated sentence or the same sentence if it was not translated before.
      *
      * @throws \ReflectionException If the class does not exist.
+     * @throws \RuntimeException    If a directory for translation files does not exists and can not be created.
      *
      * @see \ObjectModel::displayFieldName() To translate an attribute of an ObjectModel subclass.
      *
      * @author Maksim T. <zapalm@yandex.com>
      */
-    public static function translate($sentence, $class, $languageIsoCode = null)
+    public static function translate($sentence, $class, $languageIsoCode = null, $translationsDirectory = _PS_TRANSLATIONS_DIR_)
     {
         if (null === $languageIsoCode) {
             $languageIsoCode = \Context::getContext()->language->iso_code;
@@ -48,7 +50,14 @@ class TranslateHelper
         $index           = $reflectionClass->getName();
         $vocabulary      = [];
 
-        $vocabularyFilePath = _PS_TRANSLATIONS_DIR_ . $languageIsoCode . '/sentences.php';
+        $vocabularyDirectory = rtrim($translationsDirectory, '/\\') . DIRECTORY_SEPARATOR . $languageIsoCode;
+        if (false === file_exists($vocabularyDirectory)) {
+            if (false === mkdir($vocabularyDirectory, 0777, true)) {
+                throw new \RuntimeException('Can not create the directory for translation files: ' . $vocabularyDirectory);
+            }
+        }
+
+        $vocabularyFilePath = $vocabularyDirectory . DIRECTORY_SEPARATOR . 'sentences.php';
         if (file_exists($vocabularyFilePath)) {
             $vocabulary = require $vocabularyFilePath;
 

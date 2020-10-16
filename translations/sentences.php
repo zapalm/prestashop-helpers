c31abf892f23034211ff8db011e6c042f70198c1
zapalm <zapalm@ya.ru>
2020-10-16 13:18:05 +1000

    Improved AboutModuleWidget: added translations for Romanian and Swedish (along with this, the translation is now stored only in one file)

---
+++ translations/sentences.php
@@ -0,0 +1,83 @@
+<?php return array (
+  'zapalm\\prestashopHelpers\\widgets\\AboutModuleWidget' => 
+  array (
+    'Author' => 
+    array (
+      'sv' => 'Författare',
+      'ro' => 'Autor',
+      'ru' => 'Автор',
+    ),
+    'Module info' => 
+    array (
+      'sv' => 'Modulinformation',
+      'ro' => 'Informații despre modul',
+      'ru' => 'Информация о модуле',
+    ),
+    'Version' => 
+    array (
+      'sv' => 'Version',
+      'ro' => 'Versiune',
+      'ru' => 'Версия',
+    ),
+    'License' => 
+    array (
+      'sv' => 'Licens',
+      'ro' => 'Licență',
+      'ru' => 'Лицензия',
+    ),
+    'Website' => 
+    array (
+      'sv' => 'Hemsida',
+      'ro' => 'Site-ul web',
+      'ru' => 'Сайт',
+    ),
+    'Open-source license' => 
+    array (
+      'sv' => 'Licens för öppen källkodsprogramvara',
+      'ro' => 'Licență software open source',
+      'ru' => 'Открытая лицензия на ПО',
+    ),
+    'Academic Free License (AFL 3.0)' => 
+    array (
+      'sv' => 'Academic Free License 3.0 (AFL 3.0)',
+      'ro' => 'Licență academică gratuită 3.0 (AFL 3.0)',
+      'ru' => 'Академическая бесплатная лицензия 3.0 (AFL 3.0)',
+    ),
+    'Open Software License (OSL 3.0)' => 
+    array (
+      'sv' => 'Licens för öppen källkodsprogramvara (OSL 3.0)',
+      'ro' => 'Licență software open source (OSL 3.0)',
+      'ru' => 'Открытая лицензия на программное обеспечение (OSL 3.0)',
+    ),
+    'GNU General Public License (GNU 3.0)' => 
+    array (
+      'sv' => 'General Public License (GNU 3.0)',
+      'ro' => 'Licență publică generală (GNU 3.0)',
+      'ru' => 'Универсальная общественная лицензия (GNU 3.0)',
+    ),
+    'MIT License' => 
+    array (
+      'sv' => 'MIT-licens',
+      'ro' => 'Licență MIT',
+      'ru' => 'Лицензия МТИ',
+    ),
+    'Proprietary license' => 
+    array (
+      'sv' => 'Proprietary programvarulicens',
+      'ro' => 'Licență software proprietar',
+      'ru' => 'Проприетарная лицензия на ПО',
+    ),
+    'Proprietary license for closed-source software' => 
+    array (
+      'sv' => 'Proprietär licens för sluten källkodsprogramvara',
+      'ro' => 'Licență software sursă închisă',
+      'ru' => 'Проприетарная лицензия на ПО с закрытым исходным кодом',
+    ),
+    'Proprietary license for source-available software' => 
+    array (
+      'sv' => 'Proprietary programvarulicens med tillgänglig källkod',
+      'ro' => 'Licență de software proprietar cu codul sursă disponibil',
+      'ru' => 'Проприетарная лицензия на ПО с доступным исходным кодом',
+    ),
+  ),
+);
